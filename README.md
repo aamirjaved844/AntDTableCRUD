0384fe7a024d56b8037a1f1416e699da6b54ef69
Aamir Javed <aamirjaved844@gmail.com>
2022-01-22 13:58:35 +0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# AntDTableCRUD
+This repo contains code base regarding youtube tutorial (https://youtu.be/y4_nSE-aZhc) on how to implement Table CRUD like how to populate rows, add new rows, delete rows, edit rows in antd table.
+
+Notes: If somehow due to difference in react versions or so, it is not compiled correctly then you can use the code from App.js file into your project directly.
---
+++ src/App.js
@@ -0,0 +1,167 @@
+import "antd/dist/antd.css";
+import "./App.css";
+import { Button, Table, Modal, Input } from "antd";
+import { useState } from "react";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+
+function App() {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editingStudent, setEditingStudent] = useState(null);
+  const [dataSource, setDataSource] = useState([
+    {
+      id: 1,
+      name: "John",
+      email: "john@gmail.com",
+      address: "John Address",
+    },
+    {
+      id: 2,
+      name: "David",
+      email: "david@gmail.com",
+      address: "David Address",
+    },
+    {
+      id: 3,
+      name: "James",
+      email: "james@gmail.com",
+      address: "James Address",
+    },
+    {
+      id: 4,
+      name: "Sam",
+      email: "sam@gmail.com",
+      address: "Sam Address",
+    },
+  ]);
+  const columns = [
+    {
+      key: "1",
+      title: "ID",
+      dataIndex: "id",
+    },
+    {
+      key: "2",
+      title: "Name",
+      dataIndex: "name",
+    },
+    {
+      key: "3",
+      title: "Email",
+      dataIndex: "email",
+    },
+    {
+      key: "4",
+      title: "Address",
+      dataIndex: "address",
+    },
+    {
+      key: "5",
+      title: "Actions",
+      render: (record) => {
+        return (
+          <>
+            <EditOutlined
+              onClick={() => {
+                onEditStudent(record);
+              }}
+            />
+            <DeleteOutlined
+              onClick={() => {
+                onDeleteStudent(record);
+              }}
+              style={{ color: "red", marginLeft: 12 }}
+            />
+          </>
+        );
+      },
+    },
+  ];
+
+  const onAddStudent = () => {
+    const randomNumber = parseInt(Math.random() * 1000);
+    const newStudent = {
+      id: randomNumber,
+      name: "Name " + randomNumber,
+      email: randomNumber + "@gmail.com",
+      address: "Address " + randomNumber,
+    };
+    setDataSource((pre) => {
+      return [...pre, newStudent];
+    });
+  };
+  const onDeleteStudent = (record) => {
+    Modal.confirm({
+      title: "Are you sure, you want to delete this student record?",
+      okText: "Yes",
+      okType: "danger",
+      onOk: () => {
+        setDataSource((pre) => {
+          return pre.filter((student) => student.id !== record.id);
+        });
+      },
+    });
+  };
+  const onEditStudent = (record) => {
+    setIsEditing(true);
+    setEditingStudent({ ...record });
+  };
+  const resetEditing = () => {
+    setIsEditing(false);
+    setEditingStudent(null);
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button onClick={onAddStudent}>Add a new Student</Button>
+        <Table columns={columns} dataSource={dataSource}></Table>
+        <Modal
+          title="Edit Student"
+          visible={isEditing}
+          okText="Save"
+          onCancel={() => {
+            resetEditing();
+          }}
+          onOk={() => {
+            setDataSource((pre) => {
+              return pre.map((student) => {
+                if (student.id === editingStudent.id) {
+                  return editingStudent;
+                } else {
+                  return student;
+                }
+              });
+            });
+            resetEditing();
+          }}
+        >
+          <Input
+            value={editingStudent?.name}
+            onChange={(e) => {
+              setEditingStudent((pre) => {
+                return { ...pre, name: e.target.value };
+              });
+            }}
+          />
+          <Input
+            value={editingStudent?.email}
+            onChange={(e) => {
+              setEditingStudent((pre) => {
+                return { ...pre, email: e.target.value };
+              });
+            }}
+          />
+          <Input
+            value={editingStudent?.address}
+            onChange={(e) => {
+              setEditingStudent((pre) => {
+                return { ...pre, address: e.target.value };
+              });
+            }}
+          />
+        </Modal>
+      </header>
+    </div>
+  );
+}
+
+export default App;
